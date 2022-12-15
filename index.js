c74b0be54d4721d09db9b7535d857d84cabd3876
Rawllings Kiprop <rutojr01@gmail.com>
2022-12-15 16:53:06 +0300

    search bar functionality

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
-
-// const
-// const input = document.createElement("input")
-// input.id = "id"
-// input.type = "hidden"
-
-// const searchBar = document.querySelector("#search")
-// searchBar.addEventListener("input" searchButton)
-
-// function searchButton(e){
-//     e.preventDefault()
-//     const value = e.target.value
-//     console.log(value)
-// }
-
-fetch("http://localhost:3000/tenants")
-.then(res => res.json())
-.then(function(tenants){
-    console.log(tenants)
-    tenants.map(function(tenant){
-        let houseNum = document.querySelector("#housenumber")
-        const houseDisplay = document.createElement("h6")
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:3000/tenants")
+    .then((res) => res.json())
+    .then(function (tenants) {
+      // console.log(tenants)
+      tenants.map(function (tenant) {
+        let houseNum = document.querySelector("#housenumber");
+        const houseDisplay = document.createElement("h6");
         // houseDisplay.id = ("number")
-        houseNum.appendChild(houseDisplay)
-    
-        houseDisplay.innerHTML = tenant.house_name
+        houseNum.appendChild(houseDisplay);
 
-        houseDisplay.addEventListener("click", tenantDetails)
+        houseDisplay.innerHTML = tenant.house_name;
+
+        houseDisplay.addEventListener("click", tenantDetails);
 
         function tenantDetails() {
-            const tenantName = document.querySelector("#name")
-            const mobileNum = document.querySelector("#mobile")
-            const idNum = document.querySelector("#idnum")
-            const tenantAge = document.querySelector("#age")
-            const occupants = document.querySelector("#occupants")
+          const tenantName = document.querySelector("#name");
+          const mobileNum = document.querySelector("#mobile");
+          const mail = document.querySelector("#mail");
+          const idNum = document.querySelector("#idnum");
+          const tenantAge = document.querySelector("#age");
+          const occupants = document.querySelector("#occupants");
+          const visitor = document.querySelector("#visitor");
 
-            tenantName.textContent = tenant.name
-            mobileNum.textContent = tenant.mobile_number
-            idNum.textContent = tenant.id_number
-            tenantAge.textContent = tenant.age
-            occupants.textContent = tenant.occupants
-
-
-
+          tenantName.textContent = tenant.name;
+          mobileNum.textContent = tenant.mobile_number;
+          mail.textContent = tenant.email;
+          idNum.textContent = tenant.id_number;
+          tenantAge.textContent = tenant.age;
+          occupants.textContent = tenant.occupants;
+          visitor.textContent = tenant.visitors;
         }
+      });
 
+      let searchBar = document.querySelector("#search")
+      searchBar.addEventListener("keyup", searchHouse)
+
+      function searchHouse() {
+        let searchValue = searchBar.value.toUpperCase()
+        let item = document.getElementsByTagName("h6")
+
+        for(let i = 0; i<item.length; i++){
+            let span = item[i]
+            console.log(span)
     
-    })
-}
-)
+    
+            if(span.innerHTML.toUpperCase().indexOf(searchValue)> -1){
+                item[i].style.display = ""
+            }
+            else{
+                item[i].style.display = "none"
+            }
+    }
+      }
+    });
+});
